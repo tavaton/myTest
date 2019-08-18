68ecdfb3030874a079cb890bb14e030a22a707c9
Esa Raita <esa.raita@nokia.com>
2019-08-18 14:45:07 +0300

    initial stuff

---
+++ build/precache-manifest.7b61fb1463e788054266021da1d4ec34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1b41f3fa0677c3c27b49efcc1732c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f07e6119de65ed1e0bdd",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0fd92419bbd5052ffa28",
+    "url": "/static/js/2.a312e500.chunk.js"
+  },
+  {
+    "revision": "f07e6119de65ed1e0bdd",
+    "url": "/static/js/main.ff2f6e0f.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
